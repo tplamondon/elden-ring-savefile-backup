7f739e7bf1d0b2f07ee5220bbc4ef32cb50818a4
tplamondon <tplamondon@tutanota.de>
2022-02-27 18:45:18 -0800

    some more work

--- er-savemanager.py
+++ er-savemanager.py
@@ -3,7 +3,15 @@ from os import listdir
 from os.path import isfile, join
 import argparse
 from posixpath import split
+import pathlib
 
+
+#! CONSTANTS
+PROGRAMDIRECTORY = pathlib.Path(__file__).parent.resolve()
+PATH = os.getenv("APPDATA") + os.path.sep + "EldenRing"
+# this gives %appdata%/EldenRing
+SAVEFILE = "ER0000.sl2"
+SAVEFILEBAK = "ER0000.sl2.bak"
 
 # https://stackoverflow.com/questions/800197/how-to-get-all-of-the-immediate-subdirectories-in-python
 def get_immediate_subdirectories(a_dir):
@@ -12,10 +20,7 @@ def get_immediate_subdirectories(a_dir):
     ]
 
 
-def main():
-    PATH = os.getenv("APPDATA")
-    PATH = PATH + os.path.sep + "EldenRing"
-    # this gives %appdata%/EldenRing
+def getIdFolder():
     folders = get_immediate_subdirectories(PATH)
     # have a list of steam id's, ask user for which one
     for i in range(len(folders)):
@@ -27,7 +32,24 @@ def main():
         id = int(id)
         if id >= 0 and id < len(folders):
             break
-    print(id)
+    # have id
+    usePath = PATH + os.path.sep + folders[id]
+    return usePath
+
+
+def choices(folder):
+    print("Please select one of the following: ")
+    print("1. Backup Save")
+    print("2. Replace Save")
+    print("3. Select a new steam id")
+    print("4. Exit Program")
+
+
+def main():
+    folder = getIdFolder()
+    choices(folder)
+
+    #! CODE
 
 
 if __name__ == "__main__":
